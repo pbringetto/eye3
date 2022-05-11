9900553ba20046b278dfdb9fabb10d2bbce873fb
pbringetto <plbriqto@gmail.com>
2022-05-10 20:10:48 -0700

    exchange api

--- heartbeat.py
+++ heartbeat.py
@@ -1,17 +1,17 @@
 import api.ftx as f
 import cfg_load
 alpha = cfg_load.load('/home/ubuntu/eye3/alpha.yaml')
+import pandas as pd
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
 
 def go():
     ftx = f.FtxClient(alpha["key"], alpha["secret"])
 
     for pair in alpha["pairs"]:
-         return ftx.get_historical_prices(pair['pair'], 86400)
-
-
-
-
-
-print(go())
+         df = pd.DataFrame(ftx.get_historical_prices(pair['pair'], 86400))
+         print(df)
+go()
 
 
