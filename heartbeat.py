9daf6631b330281789040e2789557c181e3b5e24
pbringetto <plbriqto@gmail.com>
2022-05-10 19:48:56 -0700

    exchange api

--- heartbeat.py
+++ heartbeat.py
@@ -1,7 +1,11 @@
-def go(x):
+import api.ftx a f
+import cfg_load
+alpha = cfg_load.load('/home/ubuntu/eye3/alpha.yaml')
+
+def go(x)
+    ftx = f.FtxClient(alpha["key"], alpha["secret"])
     print(x)
 
-go(1)
-
+go('hello')
 
 
