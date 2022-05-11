ee5fa0d89e7370710c6a73af9acaf02de13cf61b
pbringetto <plbriqto@gmail.com>
2022-05-10 20:02:22 -0700

    exchange api

--- heartbeat.py
+++ heartbeat.py
@@ -2,10 +2,16 @@ import api.ftx a f
 import cfg_load
 alpha = cfg_load.load('/home/ubuntu/eye3/alpha.yaml')
 
-def go(x)
+def go()
     ftx = f.FtxClient(alpha["key"], alpha["secret"])
-    print(x)
 
-go('hello')
+    for pair in alpha["pairs"]:
+         return ftx.get_historical_prices(pair['pair'], 86400)
+
+
+
+
+
+print(go())
 
 
