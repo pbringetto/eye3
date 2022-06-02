02f0ab0c398e730cff4e716e486b4828acdf6248
pbringetto <plbriqto@gmail.com>
2022-06-01 22:05:03 -0700

    mv to package

--- strategy.py
+++ strategy.py
@@ -1,28 +1,8 @@
-import packages.indicator as i
+import ta_signals
 import pyangles
 
 class Strategy:
-    def __init__(self):
-        self.indicator = i.Indicator()
-
     def setup(self, ohlc, tf, pair):
-
-        price = float(ohlc['close'][::-1][0])
-
-        ma_data, ohlc = self.indicator.ma(ohlc)
-        ema_data, ohlc = self.indicator.ema(ohlc)
-        bollinger_data, ohlc = self.indicator.bollinger(ohlc)
-
-        rsi_data, ohlc = self.indicator.rsi(ohlc)
-        macd_data, ohlc = self.indicator.macd_slope(ohlc)
-        div_data, ohlc = self.indicator.divergence(ohlc)
-
-        obv_data, ohlc = self.indicator.on_balance_volume(ohlc, 5)
-
+        data = ta_signals.go(ohlc, 'close')
         pattern_data = pyangles.go(ohlc, 'close', [4, 4], [1, 1])
-        print(pattern_data)
-        #print(ohlc[['close', 'volume', 'bollinger_low', 'bollinger_high']].iloc[-20:])
-
-        data = div_data + bollinger_data + macd_data + ma_data + ema_data + pattern_data
-
-        return data, ohlc
+        return data + pattern_data, ohlc
